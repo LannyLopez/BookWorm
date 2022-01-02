984309b61639c0942a5212ab7f3a55f0187be760
mjlars <maflarson@gmail.com>
2022-01-02 13:12:10 -0700

    added login check and some other things into the handlebars template

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const path = require('path');
 const express = require('express');
-
 const session = require('express-session');
 const expHandlebars = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const sess1 = {
   secret: 'Definitely super secret',
+  cookie: {},
   resave: false,
   saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess1));

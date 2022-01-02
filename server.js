aef7c45ae6494eea8cb580b6c419749a98e3b48e
mjlars <maflarson@gmail.com>
2022-01-01 18:06:31 -0700

    setting up handlebars / started integrating the html into it

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
+const path = require('path');
 const express = require('express');
+
+const session = require('express-session');
+const expHandlebars = require('express-handlebars');
+
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const sess1 = {
+  secret: 'Definitely super secret',
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.use(session(sess1));
+
+const hbs = expHandlebars.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // turn on routes
 app.use(routes);

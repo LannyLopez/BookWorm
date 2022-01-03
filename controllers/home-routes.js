88876d3ebcf073321c177478349f5b74f78b801f
Spoonska <Spencer.Kyle@hotmail.com>
2022-01-02 17:07:26 -0700

    more heroku

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,45 +2,45 @@ const router = require('express').Router();
 const { Review } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/',  withAuth, async (req, res) => {
+router.get('/',  withAuth, async (req, res) => {
 
-//     try {
-//         const dbReviewData = await Review.findAll({
-//             include: [
-//                 {
-//                 model: Review,
-//                 attributes: ['username', 'comment'],
-//                 },
-//             ],
-//         }),
+    try {
+        const dbReviewData = await Review.findAll({
+            include: [
+                {
+                model: Review,
+                attributes: ['username', 'comment'],
+                },
+            ],
+        });
 
-//         const reviews = dbReviewData.map((review) => {
-//             review.get({ plain: true})
-//         });
+        const reviews = dbReviewData.map((review) => {
+            review.get({ plain: true})
+        });
 
-//         res.render('homepage', {
-//             reviews,
-//             loggedIn: req.session.loggedIn,
-//         });
-//     } catch (err) {
-//         console.log(err);
-//         res.status(500).json(err);
-//     }
+        res.render('homepage', {
+            reviews,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 
-// });
+});
 
-// router.get('/review/:id', withAuth, async (req, res) => {
-//     try {
-//       const dbSingleReviewData = await Review.findByPk(req.params.id);
+router.get('/review/:id', withAuth, async (req, res) => {
+    try {
+      const dbSingleReviewData = await Review.findByPk(req.params.id);
   
-//       const singleReview = dbSingleReviewData.get({ plain: true });
+      const singleReview = dbSingleReviewData.get({ plain: true });
   
-//       res.render('review', { singleReview, loggedIn: req.session.loggedIn });
-//     } catch (err) {
-//       console.log(err);
-//       res.status(500).json(err);
-//     }
-//   });
+      res.render('review', { singleReview, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
   
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
